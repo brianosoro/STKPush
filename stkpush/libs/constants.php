b756e7a1d0d80bddacdb63615fcbc88491e05741
brianosoro <brianosoroinc@gmail.com>
2018-01-11 17:05:27 +0300

    Update constants.php

--- stkpush/libs/constants.php
+++ stkpush/libs/constants.php
@@ -2,19 +2,19 @@
 
 define('URL', 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest');
 define('TOKEN_URL', 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials');
-define("TOKEN", "PEkasp90J9BJ0GkK8pPSpeiwWR9c");
-define('PASS_KEY', 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919');
-define('CONSUMER_KEY', 'GSY7xW3uo7ZAbSAww2MN0a28KxEw7AYC');
-define('CONSUMER_SECRET', 'FtWaG7qDn7zvZPvO');
+define("TOKEN", "ENTER TOKEN");
+define('PASS_KEY', 'ENTER PASSKEY');
+define('CONSUMER_KEY', 'ENTER CONSUMER KEY');
+define('CONSUMER_SECRET', 'ENTER SECRET KEY');
 define('SHORT_CODE', '174379');
-define('CALLBACK_URL', "http://192.81.213.230/stkpush/callback.php");
-define('SMS_USERNAME', 'lipaeasy');
-define('SMS_APIKEY', '09df0a2c5d39843f7d487dad955f243fa73e241d9ac6896729aa810e5f87aeb4');
+define('CALLBACK_URL', "");
+define('SMS_USERNAME', 'ENTER USER NAME');
+define('SMS_APIKEY', 'ENTER API KEY');
 
 
 define('DB_SERVER', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', '0722519919');
+define('DB_PASS', 'PASSWORD');
 define('DB_NAME', "stkDB");
 
 ?>
